87074653c229917511430924e0732489f2062179
tomekxf <tomekxf@gmail.com>
2013-01-16 19:54:58 +0000

    Drobne zmiany redakcyjne - pousuwane niepotrzebne komentarze + dodane ustawienie zmiennych systemowych dla SSL

--- System/EVotingCommon/src/evotingcommon/EVotingCommon.java
+++ System/EVotingCommon/src/evotingcommon/EVotingCommon.java
@@ -1,21 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package evotingcommon;
 
-/**
- *
- * @author Maciek
- */
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EVotingCommon {
+
+    public final static String MainDirectory;
+    public final static String SSLKeyAndCertStorageDir;
+    public final static String CLAKeyStoreAddr;
+    public final static String CTFKeyStoreAddr;
+    public final static String ClientTrustStoreAddr;
     
     public final static String CLAIPAddress = "localhost", CTFIPAddress = "localhost";
     public final static int CLAPortNumber = 8102, CTFPortNumber = 8103;
-    /**
-     * @param args the command line arguments
-     */
+
+    static {
+        String formalHelper = "..";
+        try {
+            formalHelper = new File("..").getCanonicalPath();
+        } catch (IOException ex) {
+            System.out.println("Problemy z ustaleniem katalogu macierzystego - sprawdz czy to oby na pewno ten, gdyz inaczej java nie odnajdzie magazynu certyfikatow.");
+            Logger.getLogger(EVotingCommon.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        MainDirectory = formalHelper;
+        SSLKeyAndCertStorageDir = MainDirectory + File.separator + "SSLKeys";//Trzeba sprawdzic czy to na pewno bedzie ten katalog w strukturze
+        CLAKeyStoreAddr = SSLKeyAndCertStorageDir + File.separator + "CLAKeyStore";
+        CTFKeyStoreAddr = SSLKeyAndCertStorageDir + File.separator + "CTFKeyStore";
+        ClientTrustStoreAddr = SSLKeyAndCertStorageDir + File.separator + "VotComCertMag";
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
     }
 }
