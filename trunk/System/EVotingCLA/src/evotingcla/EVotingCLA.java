e212b0817e4b8ff8083e8a9fb869dbd177835ae1
tomekxf@gmail.com <tomekxf@gmail.com@d231bf76-2d61-472e-4832-9ba7d6bc9372>
2013-01-17 16:31:15 +0000

    CLA uzupelnione o komunikacje z baza danych i zdalnym uzytkownikiem - do weryfikacji

--- trunk/System/EVotingCLA/src/evotingcla/EVotingCLA.java
+++ trunk/System/EVotingCLA/src/evotingcla/EVotingCLA.java
@@ -3,13 +3,15 @@ package evotingcla;
 import evotingcommon.EVotingCommon;
 import evotingcommon.RequestMessage;
 import evotingcommon.ResponseMessage;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.net.ServerSocketFactory;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
@@ -17,17 +19,52 @@ import javax.net.ssl.SSLSocket;
 
 public class EVotingCLA extends Thread {
 
-    public static void main(String[] args) {
-        
+    private static int CLA_VALIDATION_REQ = 0,
+            CTF_CANDIDATES_REQ = 0,
+            CTF_VOTE_REQ = 1,
+            CLA_OK_RESP = 0,
+            CLA_WRONG_DATA_RESP = 1,
+            CLA_SERVER_ERROR_RESP = 2,
+            CTF_OK_RESP = 0,
+            CTF_VALIDATION_INCORRECT_RESP = 1,
+            CTF_VALIDATION_USED_RESP = 2,
+            CTF_ID_USED_RESP = 3,
+            CTF_SERVER_ERROR_RESP = 4;
+    private static CLADataBase cladb;
+
+    public static void main(String[] args) throws SQLException, FileNotFoundException, ClassNotFoundException, IOException {
+
         //ustawienie zmiennych srodowiskowych dla implementacji javowej protokolu ssl
-        
+
         System.setProperty("javax.net.ssl.keyStore", EVotingCommon.SSLKeyAndCertStorageDir + "/CLAKeyStore");
         System.setProperty("javax.net.ssl.keyStorePassword", "123456");
-        
+
         //
-        
+
         ServerSocketFactory socketFactory = SSLServerSocketFactory.getDefault();
         ServerSocket serverSocket;
+
+        //Administrator podczas startu decyduje czy utworzyc nowa baze danych - w domyslnej strukturze katalogow
+        //Decyzja ta powinna byc oparta na tym czy baza juz istnieje czy jeszcze nie i musimy to zrobic
+        System.out.println("Jesli nie ma jeszcze bazy danych wpisz: utworz. W przeciwnym razie wcisnij cokolwiek.");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        if (line.equals("utworz")) {
+            System.out.println("Tworze baze danych ...");
+            CLADataBase.createDBOnDisk(EVotingCommon.CLADBAddr, EVotingCommon.CLACreatingScriptFileAddress, EVotingCommon.CLADBUsername, EVotingCommon.CLADBPassword);
+            CLADataBase.populate(EVotingCommon.CLADBAddr, EVotingCommon.CLAPopulatingScriptFileAddress, EVotingCommon.CLADBUsername, EVotingCommon.CLADBPassword);
+        } else {
+            System.out.println("Nie tworze niczego.");
+        }
+        System.out.println("Przechodze do nasluchu ...");
+
+        try {
+            cladb = new CLADataBase(EVotingCommon.CLADBAddr, EVotingCommon.CLADBUsername, EVotingCommon.CLADBPassword);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(EVotingCLA.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(EVotingCLA.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         try {
             serverSocket = (SSLServerSocket) socketFactory.createServerSocket(EVotingCommon.CLAPortNumber);
@@ -39,13 +76,12 @@ public class EVotingCLA extends Thread {
         System.out.println("Debug");
         while (true) {
             try {
-                new EVotingCLA((SSLSocket)serverSocket.accept()).start();
+                new EVotingCLA((SSLSocket) serverSocket.accept()).start();
             } catch (IOException e) {
                 System.err.println("BŁĄD: Nieudane połączenie z klientem...\n");
             }
         }
     }
-    
     private SSLSocket socket;
 
     public EVotingCLA(SSLSocket socket) {
@@ -62,14 +98,38 @@ public class EVotingCLA extends Thread {
 
             //do zaimplementowania!!!
             RequestMessage request = (RequestMessage) inputStream.readObject();
+            if (request.getType() != CLA_VALIDATION_REQ) {
+                System.out.println("Powinienem otrzymac cla_validation_req - sprawdz to");
+            }
+            String pesel = request.getData().get(0);
+            String password = request.getData().get(1);
             ResponseMessage response = new ResponseMessage();
-            response.setStatus(0);
             List<String> data = new ArrayList<String>();
-            data.add("135631265472452474525758");
             response.setData(data);
-            outputStream.writeObject(response);
+            //dowiedzmy sie dwoch rzeczy - czy te dane sa poprawne i czy wyborca odebral juz swoj nr
+            boolean registered = cladb.userAlreadyRegistered(pesel);
+            boolean matching = cladb.usernameMatchesPassword(pesel, password);
+            if (registered && matching) {
+                response.setStatus(CLA_OK_RESP);
+                Random rnd = new Random();
+                String validationNo = Long.toString(rnd.nextLong());
+                while (cladb.URIAlreadyExists(validationNo)) {
+                    validationNo = Long.toString(rnd.nextLong());
+                }
+                response.getData().add(validationNo);
+            } else {
+                response.setStatus(CLA_WRONG_DATA_RESP);
+                response.getData().add("Uztkownik ma juz swoj nr walidacyjny:" + registered + ". Hasło jest niepoprawne: " + matching);
+            }
+            /*
+             * List<String> data = new ArrayList<String>();
+             * data.add("135631265472452474525758"); response.setData(data);
+             * outputStream.writeObject(response);
+             *
+             */
 
         } catch (Exception e) {
+            System.out.println("Sprawdz czy dobrze sformulowales rzadanie.");
             ResponseMessage response = new ResponseMessage();
             response.setStatus(2);
             response.setData(null);
@@ -80,6 +140,9 @@ public class EVotingCLA extends Thread {
                 System.err.println("BŁĄD: Niedziałające połączenie - nie udało się wysłać klientowi wiadomości o wyjątku po stronie serwera...");
                 System.err.println("Błąd wystąpił przy połączeniu z klientem - IP: " + socket.getInetAddress() + " port: " + socket.getPort());
             }
+            //UWAGA!!!!
+            //ta klauzula przygotowuje sobie response ale go nie wysyla !!! Poza tym trzeba by 
+            //sprawdzic czy nie wyslano odp przed rzuceniem wyjatku bo wtedy doszloby do rozsynchronizowania komunikacji
         }
     }
 }
--- trunk/System/EVotingCommon/src/evotingcommon/EVotingCommon.java
+++ trunk/System/EVotingCommon/src/evotingcommon/EVotingCommon.java
@@ -7,11 +7,34 @@ import java.util.logging.Logger;
 
 public class EVotingCommon {
 
+    private static int CLA_VALIDATION_REQ = 0,
+            CTF_CANDIDATES_REQ = 0,
+            CTF_VOTE_REQ = 1,
+            CLA_OK_RESP = 0,
+            CLA_WRONG_DATA_RESP = 1,
+            CLA_SERVER_ERROR_RESP = 2,
+            CTF_OK_RESP = 0,
+            CTF_VALIDATION_INCORRECT_RESP = 1,
+            CTF_VALIDATION_USED_RESP = 2,
+            CTF_ID_USED_RESP = 3,
+            CTF_SERVER_ERROR_RESP = 4;
+    
+    
     public final static String MainDirectory;
     public final static String SSLKeyAndCertStorageDir;
     public final static String CLAKeyStoreAddr;
     public final static String CTFKeyStoreAddr;
     public final static String ClientTrustStoreAddr;
+    
+    public final static String CLADBAddr;
+    public final static String CTFDBAddr;
+    public final static String CLADBUsername;
+    public final static String CLADBPassword;
+    public final static String CTFDBUsername;
+    public final static String CTFDBPassword;
+    public final static String CLACreatingScriptFileAddress;
+    public final static String CTFCreatingScriptFileAddress;
+    public final static String CLAPopulatingScriptFileAddress;
     
     public final static String CLAIPAddress = "localhost", CTFIPAddress = "localhost";
     public final static int CLAPortNumber = 8102, CTFPortNumber = 8103;
@@ -29,6 +52,16 @@ public class EVotingCommon {
         CLAKeyStoreAddr = SSLKeyAndCertStorageDir + File.separator + "CLAKeyStore";
         CTFKeyStoreAddr = SSLKeyAndCertStorageDir + File.separator + "CTFKeyStore";
         ClientTrustStoreAddr = SSLKeyAndCertStorageDir + File.separator + "VotComCertMag";
+        
+        CLADBAddr = MainDirectory + File.separator + "CLADB" + File.separator + "CLADB";
+        CTFDBAddr = MainDirectory + File.separator + "CTFDB" + File.separator + "CTFDB";
+        CLADBUsername = "admin";
+        CLADBPassword = "admin1";
+        CTFDBUsername = "admin";
+        CTFDBPassword = "admin1";
+        CLACreatingScriptFileAddress = MainDirectory + File.separator + "DerbyScripts" + File.separator + "CLADBStructure";
+        CTFCreatingScriptFileAddress = MainDirectory + File.separator + "DerbyScripts" + File.separator + "CTFDBStructure";
+        CLAPopulatingScriptFileAddress = MainDirectory + File.separator  + "DerbyScripts" + File.separator + "populateCLADB";
     }
 
     public static void main(String[] args) {
