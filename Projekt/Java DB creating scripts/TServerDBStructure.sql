7aa8e09ace5f18c6bdf2195450e9a03dc28346cc
tomekxf <tomekxf@gmail.com>
2013-01-22 19:47:23 +0000

    Wersja Do Wypalenia na CD - sugestia

--- Projekt/Java DB creating scripts/TServerDBStructure.sql
+++
@@ -1,9 +0,0 @@
---the script is written in java db - also commonly known as derby
---the dialect of sql which is widely described in the internet documentation
---and which can run in two modes - either as a server or as a tool library - we choose the latter
---by saying URN we mean unique random numbers
-create table ballotBox(
-	urn varchar(20) unique,
-	rn varchar(20) unique, --the random number that voter generates himself
-	vote varchar(200) --well - that big because the vote itself will have to contain L commision's digital signature
-)
